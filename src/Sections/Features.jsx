0958f6eb056b4dcba4527cb096614bc12422e138
Tomas luquez <luquez1431@gmail.com>
2023-08-19 13:26:13 -0300

    rutas del json

--- src/Sections/Features.jsx
+++ src/Sections/Features.jsx
@@ -1,6 +1,12 @@
 import Titles from '../Components/Titles';
 import dataFeatures from '../Data/dataFeatures.json';
 import imgCircle from '../assets/img/Images/feat_circle.png';
+import imgFeatures1 from '../assets/img/Logos/fe1.png';
+import imgFeatures2 from '../assets/img/Logos/fe2.png';
+import imgFeatures3 from '../assets/img/Logos/fe3.png';
+import imgFeatures4 from '../assets/img/Logos/fe4.png';
+import imgFeatures5 from '../assets/img/Logos/fe5.png';
+
 const Features = () => {
   return (
     <section className="features pt-70 pb-70 style-4" id="features">
@@ -14,7 +20,20 @@ const Features = () => {
           {dataFeatures.data.map((feature) => (
             <div key={feature.id} className="features-card">
               <div className="icon img-contain">
-                <img src={feature.src} alt={`feature-${feature.id}`} />
+                <img
+                  src={
+                    feature.id === 1
+                      ? imgFeatures1
+                      : feature.id === 2
+                      ? imgFeatures2
+                      : feature.id === 3
+                      ? imgFeatures3
+                      : feature.id === 4
+                      ? imgFeatures4
+                      : feature.id === 5 && imgFeatures5
+                  }
+                  alt={`feature-${feature.id}`}
+                />
               </div>
               <h6>
                 {feature.text1}
